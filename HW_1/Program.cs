9264848eb3541281ac3cf7d52f5aa538ba4be8c7
Ян Гальперин <yan@MacBook-Air-An.local>
2022-08-11 18:24:32 +0300

    HW

---
+++ HW_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int numberA = ReadInt("Введите трехзначное число: ");
+int numberB = (numberA %100)/10;
+Console.WriteLine(numberB);
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
---
+++ HW_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int numberA = ReadInt("Введите число: ");
+
+if (numberA >999)
+{
+while (numberA > 999)
+{
+    numberA = numberA / 10;
+}
+
+}
+if (numberA < 100)
+{
+    Console.WriteLine(" третьей цифры нет ");
+}
+else
+{
+    Console.WriteLine(numberA %10);
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
